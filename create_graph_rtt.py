18e350d9f9086a94ec517f6c0b2754c14aa4e1cf
TobiasIlicic <93629612+TobiasIlicic@users.noreply.github.com>
2023-05-16 17:42:02 -0300

    Add files via upload

---
+++ create_graph_rtt.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.cm import get_cmap
+
+
+data = pd.read_csv("./output_geo.csv")
+df = pd.DataFrame(data)
+fig, ax = plt.subplots()
+
+# get a color map
+cmap = get_cmap("tab20")  # type: matplotlib.colors.ListedColormap
+colors = cmap.colors  # type: list
+
+ips = df['ip']
+mean_rtt = df['mean_rtt']
+
+ax.bar(ips, mean_rtt, color=colors)
+ax.set_ylabel('Mean RTT in ms')
+ax.set_title('Mean RTT for each hop between ips')
+
+
+plt.xticks(rotation=90)
+plt.show()
