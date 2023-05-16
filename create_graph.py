5b3b90882b6205bb16871a55c726d2c01de8d09e
LisandroDiMeo <ddmlisandro22@gmail.com>
2023-05-16 08:56:22 -0300

    Now is plotting in map correctly

--- create_graph.py
+++ create_graph.py
@@ -1,4 +1,5 @@
 # Importing libraries
+import matplotlib.colors
 import matplotlib.pyplot as plt
 import pandas as pd
 import geopandas as gpd
@@ -16,9 +17,14 @@ fig, ax = plt.subplots(figsize=(12, 6))
 # Plot the world map
 world.plot(ax=ax, color='lightgray')
 
-# Plot the dots on the world map
-df.plot(ax=ax, color='red', alpha=0.6, markersize=100)
+# Plot the dots on the world map with different colors based on the 'Category' column
+categories = df['ip'].unique()
+colors = list(matplotlib.colors.TABLEAU_COLORS.values())
 
-# Show the plot
+for category, color in zip(categories, colors):
+    df[df['ip'] == category].plot(ax=ax, markersize=100, color=color, alpha=0.7, label=category)
+
+# Show the plot with a legend
 plt.title('Dots on World Map')
+plt.legend()
 plt.show()
